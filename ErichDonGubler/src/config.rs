bbb53140fc0b2302c21925725947b0ecffe53558
Erich Gubler <erichdongubler@gmail.com>
2023-01-06 17:25:51 -0500

    WIP: move config.-related stuff to new `config` module

---
+++ ErichDonGubler/src/config.rs
@@ -0,0 +1,79 @@
+use serde::Deserialize;
+
+use crate::{CallStack, RefcountModifyKind, StackFrame};
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct RefcountEventParsingConfig {
+    #[serde(rename = "stack_matcher")]
+    pub stack_matchers: Vec<CallStackMatcher>,
+}
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct CallStackMatcher {
+    classification: Classification,
+    top: Vec<StackFrameMatcher>,
+}
+
+impl CallStackMatcher {
+    pub(crate) fn matches(&self, callstack: &CallStack) -> Option<Classification> {
+        let Self {
+            classification,
+            top,
+        } = self;
+        top.iter()
+            .zip(callstack.frames.iter())
+            .all(|(matcher, frame)| matcher.matches(frame))
+            .then_some(*classification)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case", tag = "type")]
+pub(crate) enum StackFrameMatcher {
+    ExternalCode,
+    Symbolicated {
+        module: Option<String>,
+        symbol_name: Option<String>,
+    },
+}
+
+impl StackFrameMatcher {
+    pub fn matches(&self, callstack: &StackFrame) -> bool {
+        match (self, callstack) {
+            (Self::ExternalCode, StackFrame::ExternalCode { .. }) => true,
+            (
+                Self::Symbolicated {
+                    module: m,
+                    symbol_name: sn,
+                },
+                StackFrame::Symbolicated {
+                    module,
+                    symbol_name,
+                    source_location: _,
+                },
+            ) => {
+                m.as_ref().map_or(true, |m| m == module)
+                    && sn.as_ref().map_or(true, |sn| sn == symbol_name)
+            }
+            _ => false,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub(crate) enum Classification {
+    Increment,
+    Decrement,
+    Destructor,
+}
+
+impl From<Classification> for RefcountModifyKind {
+    fn from(value: Classification) -> Self {
+        match value {
+            Classification::Increment => Self::Increment,
+            Classification::Decrement => Self::Decrement,
+            Classification::Destructor => Self::Destructor,
+        }
+    }
+}
--- ErichDonGubler/src/main.rs
+++ ErichDonGubler/src/main.rs
@@ -19,7 +19,10 @@ use chumsky::{
 use clap::Parser as _;
 use format::lazy_format;
 use log::LevelFilter;
-use serde::Deserialize;
+
+use crate::config::{CallStackMatcher, RefcountEventParsingConfig};
+
+mod config;
 
 #[derive(Debug, clap::Parser)]
 struct CliArgs {
@@ -73,16 +76,6 @@ enum RefcountModifyKind {
     Decrement,
     Destructor,
     Unidentified,
-}
-
-impl From<Classification> for RefcountModifyKind {
-    fn from(value: Classification) -> Self {
-        match value {
-            Classification::Increment => Self::Increment,
-            Classification::Decrement => Self::Decrement,
-            Classification::Destructor => Self::Destructor,
-        }
-    }
 }
 
 #[derive(Clone, Debug)]
@@ -365,72 +358,6 @@ struct SourceLocation {
 
 struct RefcountEventLog {
     events: Vec<Spanned<RefcountEvent>>,
-}
-
-#[derive(Debug, Deserialize)]
-struct RefcountEventParsingConfig {
-    #[serde(rename = "stack_matcher")]
-    stack_matchers: Vec<CallStackMatcher>,
-}
-
-#[derive(Debug, Deserialize)]
-struct CallStackMatcher {
-    classification: Classification,
-    top: Vec<StackFrameMatcher>,
-}
-
-impl CallStackMatcher {
-    pub fn matches(&self, callstack: &CallStack) -> Option<Classification> {
-        let Self {
-            classification,
-            top,
-        } = self;
-        top.iter()
-            .zip(callstack.frames.iter())
-            .all(|(matcher, frame)| matcher.matches(frame))
-            .then_some(*classification)
-    }
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "snake_case", tag = "type")]
-enum StackFrameMatcher {
-    ExternalCode,
-    Symbolicated {
-        module: Option<String>,
-        symbol_name: Option<String>,
-    },
-}
-
-impl StackFrameMatcher {
-    pub fn matches(&self, callstack: &StackFrame) -> bool {
-        match (self, callstack) {
-            (Self::ExternalCode, StackFrame::ExternalCode { .. }) => true,
-            (
-                Self::Symbolicated {
-                    module: m,
-                    symbol_name: sn,
-                },
-                StackFrame::Symbolicated {
-                    module,
-                    symbol_name,
-                    source_location: _,
-                },
-            ) => {
-                m.as_ref().map_or(true, |m| m == module)
-                    && sn.as_ref().map_or(true, |sn| sn == symbol_name)
-            }
-            _ => false,
-        }
-    }
-}
-
-#[derive(Clone, Copy, Debug, Deserialize)]
-#[serde(rename_all = "snake_case")]
-enum Classification {
-    Increment,
-    Decrement,
-    Destructor,
 }
 
 impl RefcountEventLog {
